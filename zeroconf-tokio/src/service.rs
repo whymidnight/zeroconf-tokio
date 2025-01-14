c5ae03e4d99ff2d1762f17723de26744d8b32097
Dominic DiGiacomo <dominic.m.digiacomo@gmail.com>
2025-01-14 03:25:03 -0500

    sync

--- zeroconf-tokio/src/service.rs
+++ zeroconf-tokio/src/service.rs
@@ -4,7 +4,7 @@ use std::sync::{Arc, Mutex};
 use std::time::Duration;
 
 use tokio::sync::oneshot;
-use zeroconf::{prelude::*, EventLoop};
+use zeroconf::prelude::*;
 use zeroconf::{MdnsService, ServiceRegistration};
 
 use crate::event_processor::EventProcessor;
@@ -64,11 +64,6 @@ impl MdnsServiceAsync {
     /// Start the service.
     pub async fn start(&mut self) -> zeroconf::Result<ServiceRegistration> {
         self.start_with_timeout(Duration::ZERO).await
-    }
-
-    /// Start the service.
-    pub async fn event_loop(&mut self) -> zeroconf::Result<EventLoop> {
-        self.inner.register()
     }
 
     /// Shutdown the service.
